758fff9ed4a9423496fd64c4473b39959b6c7f50
Rustem <lifelikechoco@yandex.ru>
2024-03-16 13:13:40 +0300

    Add getRawByRustem method

--- src/main/java/dev/masfuj/training/TrainingApplication.java
+++ src/main/java/dev/masfuj/training/TrainingApplication.java
@@ -9,6 +9,10 @@ public class TrainingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
 		System.out.println("This is Rustem");
+		System.out.println(getRawByRustem());
 	}
 
+	public static String getRawByRustem () {
+		return "Строка 2";
+	}
 }
